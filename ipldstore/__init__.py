824afdc65c084108cd468563fc9cd1c0f25500bb
Dan Parangi <dmp267@cornell.edu>
2023-11-28 09:22:48 -0500

    release/v1.0.0 changes

--- ipldstore/__init__.py
+++ ipldstore/__init__.py
@@ -5,12 +5,24 @@ IPLD stores for zarr
 from .ipldstore import IPLDStore
 from .contentstore import ContentAddressableStore, MappingCAStore, IPFSStore
 
+import os
 
-def get_ipfs_mapper(host: str = "http://127.0.0.1:5001",
-                    max_nodes_per_level: int = 10000,
-                    chunker: str = "size-262144",
-                    should_async_get: bool = True) -> IPLDStore:
+# IPFS_HOST = "http://127.0.0.1:5001"
+# IPFS_HOST = "http://172.17.0.6:5001"
+
+
+
+def get_ipfs_mapper(
+    host: str = "http://127.0.0.1:5001",
+    max_nodes_per_level: int = 10000,
+    chunker: str = "size-262144",
+    should_async_get: bool = True,
+) -> IPLDStore:
     """
     Get an IPLDStore for IPFS running on the given host.
     """
+    host_from_env = os.getenv("IPFS_HOST")
+    if host_from_env:
+        host = host_from_env
+
     return IPLDStore(IPFSStore(host, chunker=chunker, max_nodes_per_level=max_nodes_per_level), should_async_get=should_async_get)
